946a81b8fbab9bc7b46718745006c10cf45d5e51
stvanderhelm <0929380@hr.nl>
2017-09-29 11:09:51 +0200

    Added base design of the webstore

---
+++ store/templatetags/site_conf.py
@@ -0,0 +1,9 @@
+from django import template
+from django.conf import settings
+
+register = template.Library()
+
+# website global settings
+@register.simple_tag
+def PRIMARY_COLOR():
+    return "green"

ea617523a354780979cd5cf5c3aad21e51dd6941
RektInator <mlmmlangeveld@gmail.com>
2017-10-04 15:59:52 +0200

    Post shit

--- store/views/account.py
+++ store/views/account.py
@@ -2,12 +2,20 @@ from django.http import HttpResponse
 from django.shortcuts import render
 from django.template import loader
 from django.template.context import RequestContext
+from . import renderer
 
 def overview(request):
-    return render(request, 'store/account/overview.html')
+    return renderer.RenderWithContext(request, 'store/account/overview.html')
 
 def register(request):
-    return render(request, 'store/account/register.html')
+    return renderer.RenderWithContext(request, 'store/account/register.html')
 
 def login(request):
-    return render(request, 'store/account/login.html')
+    if request.method == 'POST':
+        
+        email = request.POST.get("email", "")
+        password = request.POST.get("password", "")
+
+        return HttpResponse(username)
+    else:
+        return renderer.RenderWithContext(request, 'store/account/login.html')
--- store/views/products.py
+++ store/views/products.py
@@ -2,9 +2,10 @@ from django.http import HttpResponse
 from django.shortcuts import render
 from django.template import loader
 from django.template.context import RequestContext
+from . import renderer
 
 def item(request):
-    return render(request, 'store/products/item.html')
+    return renderer.RenderWithContext(request, 'store/products/item.html')
 
 def list(request):
-    return render(request, 'store/products/products.html')
+    return renderer.RenderWithContext(request, 'store/products/products.html')

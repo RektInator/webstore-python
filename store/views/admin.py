1db450fea7815f7f2dac395639cc2635342d3b5d
RektInator <mlmmlangeveld@gmail.com>
2017-12-20 14:40:47 +0100

    WIshlist fixed

--- store/views/admin.py
+++ store/views/admin.py
@@ -20,6 +20,27 @@ def remove(request, id):
 def user(request, id):
     try:
         user = models.Accounts.objects.get(id=id)
+
+        if request.method == 'POST':
+            fullname = request.POST.get("name", "")
+            email = request.POST.get("email", "")
+            password = request.POST.get("password", "")
+            administrator = request.POST.get("admin", "off")
+
+            if len(fullname) > 0:
+                user.fullname = fullname
+            if len(email) > 0:
+                user.email = email
+            if len(password) > 0:
+                # todo!
+                pass
+            if administrator == "on":
+                user.administrator = 1
+            else:
+                user.administrator = 0
+            
+            user.save()
+
         return renderer.RenderWithContext(request, 'store/admin/user.html', {
             "user": user
         })
